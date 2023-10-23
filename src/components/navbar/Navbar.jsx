65a34210dfc276a77aa980237e9d163315178c9a
aamiryaqoob <aamiryaqoob965@gmail.com>
2023-10-24 02:00:35 +0500

    Updated

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,8 +1,11 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './navbar.css';
-import { RiMenu3Line, RiCloseLin } from 'react-icons/ri';
+import { RiMenu3Line, RiCloseLine } from 'react-icons/ri';
 import logo from '../../assests/logo.svg';
+
+
 const Navbar = () => {
+  const [toggleMenu, setToggleMenu] = useState(false);
   return (
     <div className='gpt3__navbar'>
       <div className='gpt3__navbar-links'>
@@ -13,9 +16,19 @@ const Navbar = () => {
           <p><a href="#home">Home</a></p>
           <p><a href="#wgpt3">What is GPT3?</a></p>
           <p><a href="#possibility">Open AI</a></p>
-          <p><a href="#featres">Case Studies</a></p>
+          <p><a href="#features">Case Studies</a></p>
           <p><a href="#blog">Library</a></p>
         </div>
+      </div>
+      <div className='gpt3__navbar-sign'>
+        <p>Sign in</p>
+        <button type='button'>Sign up</button>
+      </div>
+      <div className='gpt3__navbar-menu'>
+        {toggleMenu
+        ? <RiCloseLine color='#fff' size={27} onClick={() => setToggleMenu(false)} />
+        : <RiMenu3Line color='#fff' size={27} onClick={() => setToggleMenu(true)} />
+        }
       </div>
     </div>
   )
